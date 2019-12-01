fb3dc52059a51e7eecc65e7998d778c80693503b
Coder <coder@dev.lan>
2019-12-01 23:41:01 +0000

    for training, ignore messages that involve using the ditto bot

--- ditto/ditto/slack_client.py
+++ ditto/ditto/slack_client.py
@@ -5,6 +5,7 @@ class SlackClient:
     def __init__(self):
         token = os.environ["OAUTH"]
         self._client = slack.WebClient(token=token)
+        self._self_user_id = self._client.auth_test()["user_id"]
 
     def post_message(self, channel_id, text):
         self._client.chat_postMessage(channel=channel_id, text=text)
@@ -24,9 +25,18 @@ class SlackClient:
             return
 
         for message_body in messages_chunk:
-            if message_body.get("user") == user_id and message_body["type"] == "message":
+            if _message_is_valid(user_id, self._self_user_id, message_body):
                 yield message_body["text"]
 
         if response["has_more"]:
             new_start_ts = messages_chunk[-1]["ts"]
             yield from self._scrape_messages(channel_id, user_id, new_start_ts)
+
+def _message_is_valid(user_id, ditto_user_id, message_body):
+    if message_body["type"] != "message":
+        return False
+
+    is_correct_user = message_body.get("user") == user_id
+    is_not_for_ditto = not message_body["text"].lstrip().startswith(f"@<{ditto_user_id}>")
+
+    return is_correct_user and is_not_for_ditto
